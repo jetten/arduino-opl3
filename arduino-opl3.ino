3f852a1a53aa8ba27f8ab346bf1022b4ca52a8e1
Rasmus Eskola <fruitiex@gmail.com>
2015-10-08 14:27:54 +0300

    WIP: skip CS/WR pins

--- arduino-opl3.ino
+++ arduino-opl3.ino
@@ -1,84 +1,57 @@
-char inputhex[2];
+char hex_s[2];
 byte data = 0;
-byte syncdata = 0;
+byte io_addr = 0;
 
-boolean addresswrite = true;
+// pins 2 - 9 are used for the ISA data bus TODO: don't hardcode these
 
 byte A = 10; // A0 Address select input
-byte CS = 11; // CS Chip select
-byte WR = 12; // WR Write enable
-byte B = 13; // A1 Address select input
+byte B = 11; // A1 Address select input
 
+// TODO: IOWRITE writes are hardcoded at the moment
+byte IOWRITE = 12; // ISA I/O write pin
 
 void setup() {
-  
   Serial.begin(1000000);
+
   // Set port D & B to outputx
   DDRD = DDRD | B11111111;
   DDRB = DDRB | B11111111;
-  
-  pinMode(A,OUTPUT); // A0 Address select input
-  pinMode(B,OUTPUT); // A1 Address select input
-  pinMode(CS,OUTPUT); // CS Chip select
-  pinMode(WR,OUTPUT); // WR Write enable
 
-  digitalWrite(A,LOW);
-  digitalWrite(B,LOW);
-  digitalWrite(CS,HIGH);
-  digitalWrite(WR,LOW);
-  
+  pinMode(A, OUTPUT); // A0 Address select input
+  pinMode(B, OUTPUT); // A1 Address select input
+
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
 }
 
 void loop() {
-
-  
-  while(true) {
-    syncdata = Serial.read();
-    if(syncdata >= 0 && syncdata <=3) {
+  while (true) {
+    // io_addr is sent as (port - 0x388), the opl3 responds to addresses:
+    // 0x388, 0x389, 0x39a, 0x39b,
+    // so we only need to change the latter two bits of the I/O address
+    io_addr = Serial.read();
+    if (io_addr >= 0 && io_addr <= 3) {
       break;
     }
-  } 
-    // Read 2 characters from the serial buffer into inputhex
-    Serial.readBytes(inputhex,2);
+  }
 
-    // convert string inputhex to byte
-    data = strtol(inputhex, NULL, 16);
+  // read two characters as hex string
+  Serial.readBytes(hex_s, 2);
 
-    if(addresswrite) {
-      //A LOW, CS HIGH
-      PORTB = B00001000;
-      
-    }
-    else {
-      //A HIGH, CS HIGH
-      PORTB = B00001100;
-    }
+  // set IOWRITE to LOW
+  PORTB = B11101111;
 
-    // 10ns delay
-    // Write CS LOW
-    PORTB = PORTB & B11110111;
+  // convert hex_s to byte
+  data = strtol(hex_s, NULL, 16);
 
-    // 10ns delay
-    // WR LOW (it is already!)
-    // PORTB = PORTB & B11101111;
+  PORTB = io_addr << 2;
 
-    // Uuh DATAA :d
-    PORTD = data << 2;
-    PORTB = data >> 6;
-    
-    // 100 ns delay
-    
+  // write data to the data pins
+  PORTD = data << 2;
+  PORTB = data >> 6;
 
-    // CS HIGH, data reading will happen now :DD:D
-    PORTB = PORTB | B00001000;
+  // 100 ns delay
 
-
-    // Invert addresswrite
-    if(addresswrite) {addresswrite = false;}
-    else {addresswrite = true;}
-    
-  
-
-  
-
+  // write IOWRITE pin HIGH
+  PORTB = PORTB | B00010000;
 }
